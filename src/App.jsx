57d11957b7f1a8817d24475080fabea375de42aa
Lochana-susith <lochanasusith4@gmail.com>
2026-08-27 16:30:19 +0530

    create alert🤞 loginfunction👍 learn usestate✨

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import LoginPage from "./assets/pages/login"
 import RegisterPage from "./assets/pages/register"
 import AdminPage from "./assets/pages/admin"
 import { Routes, Route } from "react-router-dom"
+import { Toaster } from "react-hot-toast"
     
 
 export default function App() {
 
   return (
     <div className="w-full h-screen bg-primary text-secondary" >
+
+      <Toaster position="top-center"/>
       <Routes>
         <Route path="/" element={<HomePage/>}></Route>
         <Route path="/login" element={<LoginPage/>}></Route>
--- src/assets/pages/login.jsx
+++ src/assets/pages/login.jsx
@@ -1,6 +1,41 @@
 import { Link } from "react-router-dom";
+import {useState} from "react";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+
+
 
 export default function LoginPage() {
+
+  const [email,setEmail] = useState(" ");
+  const [password,setPassword] = useState(" ");
+
+  // function login(){
+  //   console.log(email,password);
+  //   axios.post("http://localhost:3000/users/login",{
+  //     email:email,
+  //     password:password
+  //   }).then(()=>{
+  //     console.log("login successfull")
+  //   }).catch(()=>{
+  //     console.log("login failed")
+  //   })
+  // }
+
+  async function login(){
+    try{
+      const res = await axios.post("http://localhost:3000/users/login",{
+        email:email,
+        password:password
+      })
+      console.log(res);
+      toast.success("Login successful!");
+    }catch(err){
+      console.log(err);
+      toast.error("Login failed!");
+    }
+
+  }
 
   return (
     <div className="w-full h-full bg-[url(/i-bg.png)] bg-cover bg-center bg-no-repeat flex ">
@@ -16,10 +51,12 @@ export default function LoginPage() {
 
           <div className="w-[400px] h-[500px] backdrop-blur-md shadow-2xl rounded-lg flex flex-col justify-center ">
 
-           <input type="email" placeholder="Email" className="w-[90%] h-[50px] bg-transparent border border-primary rounded-sm placeholder:text-primary text-secondary focus:outline-none m-3 p-2"></input>
-           <input type="password" placeholder="Password" className="w-[90%] h-[50px] bg-transparent border border-primary rounded-sm placeholder:text-primary text-secondary focus:outline-none m-3 p-2"></input>
+           <input type="email" placeholder="Email" className="w-[90%] h-[50px] bg-transparent border border-primary rounded-sm placeholder:text-primary text-secondary focus:outline-none m-3 p-2" onChange={(e)=>{setEmail(e.target.value)}}/>
+
+           <input type="password" placeholder="Password" className="w-[90%] h-[50px] bg-transparent border border-primary rounded-sm placeholder:text-primary text-secondary focus:outline-none m-3 p-2" onChange={(e)=>{setPassword(e.target.value)}}/>
+
            <p className="m-3 w-[90%] text-right text-secondary">Forget password? <Link to="/register" className="text-accent font-bold">Reset</Link></p>
-           <button className="m-3 p-3 w-[90%] h-[50px] bg-accent rounded-lg text-secondary font-bold">Login</button>
+           <button className="m-3 p-3 w-[90%] h-[50px] bg-accent rounded-lg text-secondary font-bold" onClick={login}>Login</button>
            <button className="m-3 p-3 w-[90%] h-[50px] border border-accent rounded-lg text-secondary font-bold">Login with google</button>
            <p className="m-3 w-[90%] text-right text-secondary">Don't have an account? <Link to="/register" className="text-accent font-bold">Register</Link></p>
            
